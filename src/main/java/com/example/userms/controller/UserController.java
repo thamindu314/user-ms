35620d2ab79c00123fb4e57693cbf6183286f260
Thamindu Perera <denuwan41.perera@gmail.com>
2024-04-05 15:15:57 +0530

    Update CrossOrigin

--- src/main/java/com/example/userms/controller/UserController.java
+++ src/main/java/com/example/userms/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:3000")
+
 @RestController
 public class UserController {
 
