ed42dfba16c09b40ea3d485e26695f874ffb0c56
Kumara <CODSE222F-014@student.nibm.lk>
2024-04-05 10:38:23 +0530

    I Added CrossOrigin

--- src/main/java/com/example/userms/controller/UserController.java
+++ src/main/java/com/example/userms/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 public class UserController {
 
